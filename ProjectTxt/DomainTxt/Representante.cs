e5a77cd4aa6dbd6e5a6aa9591d27e1564def4042
diogodamato <diogo.damato@hotmail.com>
2013-08-29 17:15:44 -0300

    ProjectTxt Inicio

---
+++ ProjectTxt/DomainTxt/Representante.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainTxt
+{
+    public class Representante
+    {
+    }
+}
